e63dcce637a246180e7f2b3405538f714b3eaf72
Gerard <gkhynes@gmail.com>
2017-07-22 21:59:21 +0100

    Add webpack

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,10 @@
+function Person(fullName, favColour) {
+  this.name = fullName;
+  this.favouriteColour = favColour;
+  this.greet = function() {
+    console.log("Hello, my name is " + this.name + " and my favourite colour is " + this.favouriteColour + ".");
+  }
+}
+
+
+module.exports = Person;
