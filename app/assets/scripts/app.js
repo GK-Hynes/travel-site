1c354efef872a2d43951030d424f856c49f0d09e
Gerard <gkhynes@gmail.com>
2017-07-23 17:33:20 +0100

    Automate webpack

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,8 @@
 var $ = require("jquery");
 var Person = require("./modules/Person");
 
+alert("Testing");
+
 var john = new Person("John Doe", "blue");
 john.greet();
 
--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -70,13 +70,13 @@
 var $ = __webpack_require__(1);
 var Person = __webpack_require__(2);
 
+alert("Testing");
+
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
-
-$("h1").remove();
 
 
 /***/ }),
