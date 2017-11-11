e055162f8af19114dd6be06ebe25921d6159f69e
Gerard <gkhynes@gmail.com>
2017-11-11 18:06:49 +0000

    Add README.md

---
+++ README.md
@@ -0,0 +1,52 @@
+# Clear View Escapes
+
+### A sample travel site built for Brad Schiff's _Git a Web Developer Job: Mastering the Modern Workflow_ course. 
+
+![Clear View Escapes Screenshot](https://screenshots.firefox.com/j1y08aDnQBYIGamo/gk-hynes.github.io)
+
+## Built with:
+
+* Git - for version control
+* Node - to automate tasks
+* Gulp - 
+* PostCSS - 
+* Browsersync - 
+* Webpack - 
+* Babel - 
+* Waypoints
+* Flexbox - 
+
+## Skills learned 
+
+### Version Control with Git
+I went from knowing the basics of Git to making it part of my daily routine. 
+
+### Mobile-first Development
+
+* #### Custom CSS
+    The site features a custom-built, responsive 12 column grid without using Bootstrap. 
+
+* #### BEM
+    Super organized CSS using the BEM methodology. 
+
+* #### Flexbox
+
+### Automating build tasks with Gulp
+I used Gulp to automate everything. SVG sprites. imagemin
+
+### Organized, modern JavaScript
+Super organized, modular, ES6 JavaScript. 
+
+### Lazy loading 
+Speed up load times and save users' data plans by only loading images when the user scrolls to that point. 
+
+### Legacy Browser Support
+
+### Deploying sites to GitHub pages
+You can see a live version of the site running [here](https://gk-hynes.github.io/travel-site/)
+
+
+
+
+
+
--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,11 @@
-var gulp         = require("gulp"),
-    imagemin     = require("gulp-imagemin"),
-    del          = require("del"),
-    usemin       = require("gulp-usemin"),
-    rev          = require("gulp-rev"),
-    cssnano      = require("gulp-cssnano"),
-    uglify       = require("gulp-uglify"),
-    browserSync  = require("browser-sync").create();
+var gulp = require("gulp"),
+  imagemin = require("gulp-imagemin"),
+  del = require("del"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  cssnano = require("gulp-cssnano"),
+  uglify = require("gulp-uglify"),
+  browserSync = require("browser-sync").create();
 
 gulp.task("previewDist", function() {
   browserSync.init({
@@ -29,19 +29,25 @@ gulp.task("copyGeneralFiles", ["deleteDistFolder"], function() {
     "!./app/assets/scripts/**",
     "!./app/temp",
     "!./app/temp/**"
-  ]
+  ];
 
-  return gulp.src(pathsToCopy)
-  .pipe(gulp.dest("./docs"));
+  return gulp.src(pathsToCopy).pipe(gulp.dest("./docs"));
 });
 
 gulp.task("optimizeImages", ["deleteDistFolder"], function() {
-  return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons", "!./app/assets/images/icons/**/*"])
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      multipass: true
-    }))
+  return gulp
+    .src([
+      "./app/assets/images/**/*",
+      "!./app/assets/images/icons",
+      "!./app/assets/images/icons/**/*"
+    ])
+    .pipe(
+      imagemin({
+        progressive: true,
+        interlaced: true,
+        multipass: true
+      })
+    )
     .pipe(gulp.dest("./docs/assets/images"));
 });
 
@@ -50,12 +56,34 @@ gulp.task("useminTrigger", ["deleteDistFolder"], function() {
 });
 
 gulp.task("usemin", ["styles", "scripts"], function() {
-  return gulp.src("./app/index.html")
-    .pipe(usemin({
-      css: [function() {return rev()}, function() {return cssnano()}],
-      js: [function() {return rev()}, function() {return uglify()}]
-    }))
+  return gulp
+    .src("./app/index.html", "./README.md")
+    .pipe(
+      usemin({
+        css: [
+          function() {
+            return rev();
+          },
+          function() {
+            return cssnano();
+          }
+        ],
+        js: [
+          function() {
+            return rev();
+          },
+          function() {
+            return uglify();
+          }
+        ]
+      })
+    )
     .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("build", ["deleteDistFolder", "copyGeneralFiles", "optimizeImages", "useminTrigger"]);
+gulp.task("build", [
+  "deleteDistFolder",
+  "copyGeneralFiles",
+  "optimizeImages",
+  "useminTrigger"
+]);
